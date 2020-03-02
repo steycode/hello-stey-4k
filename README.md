f8e7cfd13fd38870d6a03e7bb312f69bf75ed60e
yoann CAPLAIN <yoann.caplain@free.fr>
2020-03-02 19:25:41 +0800

    base

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
-# hello-stey-4k
+Project to introduce kotlin, Spring Boot, and more to build Stey tech stacks, to new comers.
+
+---
+
+For job applicants
+ 1. Clone the project
+ 2. Change default serialization of dates format (long or different string format)
+ 3. Change default deserialization of dates format (long or different string format)
+ 4. Add new serializer and deserializer in JSON for Pageable and Sort of Spring Data
+ 5. Change default pageable parameters (size, page, param name, etc)
+ 6. Issue a pull request for review
---
+++ src/main/kotlin/com/stey/hello/SimpleController.kt
@@ -0,0 +1,33 @@
+package com.stey.hello
+
+import org.springframework.data.domain.Pageable
+import org.springframework.data.domain.Sort
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestParam
+import org.springframework.web.bind.annotation.RestController
+import java.time.Instant
+
+/**
+ * Created on 2020/03/02.
+ *
+ * @author Yoann CAPLAIN
+ */
+@RestController("/api")
+class SimpleController {
+
+    @GetMapping("/read-serialize")
+    fun readAndSerializeDate(@RequestParam time: Instant): Instant {
+        return time
+    }
+
+    @GetMapping("do-pageable")
+    fun pageable(pageable: Pageable): Pageable {
+        return pageable
+    }
+
+    @GetMapping("do-sort")
+    fun sort(sort: Sort): Sort {
+        return sort
+    }
+
+}
