753759e3c9400de0a13cd89b0dacc546d03ecdc3
yoann CAPLAIN <yoann.caplain@free.fr>
2020-03-02 19:31:26 +0800

    base

--- src/main/kotlin/com/stey/hello/DemoApplication.kt
+++ src/main/kotlin/com/stey/hello/DemoApplication.kt
@@ -1,9 +1,11 @@
 package com.stey.hello
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration
+import org.springframework.boot.autoconfigure.jdbc.DataSourceTransactionManagerAutoConfiguration
 import org.springframework.boot.runApplication
 
-@SpringBootApplication
+@SpringBootApplication(exclude = [DataSourceAutoConfiguration::class, DataSourceTransactionManagerAutoConfiguration::class])
 class DemoApplication
 
 fun main(args: Array<String>) {
